2801e4a99efd8cec2eb67c9f42a411b898973311
Andrew Ralon <andrew.ralon@golfchannel.com>
2020-07-12 18:39:34 -0400

    BOOYAH-13 Get local PC's internal IP addresses (supports multiple network interfaces)

---
+++ Get-IPs.bat
@@ -0,0 +1,10 @@
+@ECHO off
+SETLOCAL
+SETLOCAL enabledelayedexpansion
+FOR /f "usebackq tokens=2 delims=:" %%a IN (`ipconfig ^| findstr /r "IPv4.*[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"`) DO (
+  SET _temp=%%a
+  REM Remove leading space
+  SET _ipaddress=!_temp:~1!
+  ECHO !_ipaddress!
+  )
+ENDLOCAL
