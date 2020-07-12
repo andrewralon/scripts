2fb0b889820d5a47b128a1708515cd9eb5ca2acd
Andrew Ralon <andrew.ralon@golfchannel.com>
2020-07-12 10:53:04 -0400

    BOOYAH-9 electronic mailz

---
+++ Send-EmailViaMandrill.ps1
@@ -0,0 +1,36 @@
+# Send-EmailViaMandrill.ps1
+# References:
+#  * https://stackoverflow.com/questions/38012564/how-do-i-correctly-convert-a-hashtable-to-json-in-powershell
+
+# Force TLS 1.2 assuming older protocols are disabled
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$apiKey = "REDACTED"
+$url = "https://mandrillapp.com/api/1.0/messages/send.json"
+$headers = @{ }
+
+$fromEmail = "firstname.lastname@company.com"
+$fromName = "Lastname, Firstname"
+$to = @( "firstname1.lastname2@company.com" )
+$subject = "Email Subject"
+$emailBody = "HTML Body"
+
+$requestBody = @{
+	key     = "$apiKey"
+	message = @{
+		subject    = "$subject"
+		html       = "$emailBody"
+		from_email = "$fromEmail"
+		from_name  = "$fromName"
+		to         = @(
+			@{
+				email = "$to"
+				type  = "to"
+			}
+		)
+	}
+} | ConvertTo-Json -Depth 4
+
+Write-Output $requestBody
+
+Invoke-RestMethod $url -Method Post -Headers $headers -ContentType "application/json" -Body $requestBody
---
+++ Send-EmailViaSMTP.ps1
@@ -0,0 +1,19 @@
+# Send-EmailViaSMTP.ps1
+# Force TLS 1.2 assuming older protocols are disabled
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$smtpServer = "smtpserver.company.com"
+$smtpPort = 25
+
+$from = "Lastname, Firstname <firstname.lastname@company.com>"
+$to = @( 
+	"Lastname2, Firstname2 <firstname2.lastname2@company.com>" 
+)
+$cc = @( 
+	"Lastname3, Firstname3 <firstname3.lastname3@company.com>",
+	"Lastname4, Firstname4 <firstname4.lastname4@company.com>"
+)
+$subject = "Email Subject"
+$body = "HTML Body"
+
+Send-MailMessage -From $from -To $to -Cc $cc -Subject $subject -Body $body -BodyAsHtml -SmtpServer $smtpServer -Port $smtpPort
